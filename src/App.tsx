fab62d65b015e171db6e67f5c38bc1c97bf9163c
ziayamin <ziayamin8@gmail.com>
2023-02-07 21:52:17 +0430

    clear the interface and pass to child component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 
 import { useState } from 'react';
 import './App.css';
+import List from "./components/List";
+
+interface IState {
+  people:{
+    name:string,
+    age:number,
+    url:string,
+    note?:string
+  }[]
+}
 
 function App() {
-  const [people,setPoeple]=useState({
-    name:"James",
-    url:"",
-    age:36,
-    note:"",
-  })
+  const [people,setPoeple]=useState<IState["people"]>([])
   return (
     <div className="App">
       <h1>People who are invited to my party.</h1>
+      <List people={people} />
     </div>
   );
 }
